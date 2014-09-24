59d8c69e65c3834dfcc2661576b1da8fc345c5b0
yinduo <397175732@qq.com>
2014-09-24 16:35:00 +0800

    add custom DrawableBottom attrs

--- foldable-textview/library/src/main/java/com/yinduo527/foldabletextview/library/FoldableTextView.java
+++ foldable-textview/library/src/main/java/com/yinduo527/foldabletextview/library/FoldableTextView.java
@@ -1,9 +1,11 @@
 package com.yinduo527.foldabletextview.library;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.text.Layout;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,16 +19,24 @@ public class FoldableTextView extends TextView implements View.OnClickListener {
 
     private boolean isEllipsized = false;
 
+    private int foldingDrawableBottom = 0;
+    private int unfoldingDrawableBottom = 0;
+
     public FoldableTextView(Context context) {
         super(context);
     }
 
     public FoldableTextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public FoldableTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FoldableTextView, defStyle, 0);
+
+        foldingDrawableBottom = a.getResourceId(R.styleable.FoldableTextView_foldingDrawableBottom, 0);
+        unfoldingDrawableBottom = a.getResourceId(R.styleable.FoldableTextView_unfoldingDrawableBottom, 0);
+
     }
 
 
@@ -40,7 +50,7 @@ public class FoldableTextView extends TextView implements View.OnClickListener {
                 if (maxLines > 0) {
                     if (l.getEllipsisCount(maxLines - 1) > 0) {
                         isEllipsized = true;
-                        setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                        setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, foldingDrawableBottom);
                         setOnClickListener(this);
                         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
                         requestLayout();
@@ -53,9 +63,21 @@ public class FoldableTextView extends TextView implements View.OnClickListener {
 
     }
 
+    public void setFoldingDrawableBottom(int foldingDrawableBottom) {
+        this.foldingDrawableBottom = foldingDrawableBottom;
+        requestLayout();
+        invalidate();
+    }
+
+    public void setUnfoldingDrawableBottom(int unfoldingDrawableBottom) {
+        this.unfoldingDrawableBottom = unfoldingDrawableBottom;
+        requestLayout();
+        invalidate();
+    }
+
     @Override
     public void onClick(View v) {
-        setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, isEllipsized ? 0 : 0);
+        setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, isEllipsized ? unfoldingDrawableBottom : foldingDrawableBottom);
         setEllipsize(isEllipsized ? null : TextUtils.TruncateAt.END);
         setMaxLines(isEllipsized ? Integer.MAX_VALUE : maxLines);
         isEllipsized = !isEllipsized;
